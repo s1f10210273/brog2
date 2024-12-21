19892b9f8e4b802b246588521effebb4b6890509
s1f10210273 <s1f102102739@iniad.org>
2024-12-22 04:55:48 +0900

    ローディングの修正

--- src/app/post/add/page.tsx
+++ src/app/post/add/page.tsx
@@ -2,62 +2,65 @@
 import useUser from "@/hooks/useUser";
 import useAddPostForm from "@/hooks/post/useAddPostForm";
 import FullScreenLoader from "@/components/FullScreenLoader";
+import { useRouter } from "next/navigation";
 
 const AddPostPage = () => {
   const { session, loading: userLoading } = useUser();
 
   const { register, handleSubmit, errors, isPending } = useAddPostForm();
-
+  const router = useRouter();
   const loading = userLoading || isPending;
-  if (!session)
-    return (
-      <main className="flex justify-center items-center h-screen">
-        <p>
-          <a href="/user/login" className="text-blue-500 hover:underline">
-            ログインしてね！
-          </a>
-        </p>
-      </main>
-    );
+  if (loading) return <FullScreenLoader loading />;
+
+  if (loading && !session) {
+    router.push("/");
+  }
 
   return (
-    <main className="max-w-md mx-auto p-6">
-      <FullScreenLoader loading={loading} />
-      <p className="text-2xl font-bold mb-4">Add Post</p>
+    <>
+      {session && (
+        <main className="max-w-md mx-auto p-6">
+          <p className="text-2xl font-bold mb-4">Add Post</p>
 
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <input
-            id="title"
-            placeholder="タイトル"
-            {...register("title")}
-            className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:border-blue-500"
-          />
-          {errors.title && (
-            <div className="text-red-500 text-sm">{errors.title.message}</div>
-          )}
-        </div>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <input
+                id="title"
+                placeholder="タイトル"
+                {...register("title")}
+                className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:border-blue-500"
+              />
+              {errors.title && (
+                <div className="text-red-500 text-sm">
+                  {errors.title.message}
+                </div>
+              )}
+            </div>
 
-        <div className="mb-4">
-          <input
-            id="content"
-            placeholder="内容"
-            {...register("content")}
-            className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:border-blue-500"
-          />
-          {errors.content && (
-            <div className="text-red-500 text-sm">{errors.content.message}</div>
-          )}
-        </div>
+            <div className="mb-4">
+              <input
+                id="content"
+                placeholder="内容"
+                {...register("content")}
+                className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:border-blue-500"
+              />
+              {errors.content && (
+                <div className="text-red-500 text-sm">
+                  {errors.content.message}
+                </div>
+              )}
+            </div>
 
-        <button
-          type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-500"
-        >
-          追加
-        </button>
-      </form>
-    </main>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-500"
+            >
+              追加
+            </button>
+          </form>
+        </main>
+      )}
+    </>
   );
 };
 
--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,45 +1,47 @@
 "use client";
 import useUser from "@/hooks/useUser";
 import { PostType } from "@/types/post";
-import Link from "next/link"; // Linkコンポーネントをインポート
+import Link from "next/link";
+import FullScreenLoader from "@/components/FullScreenLoader";
+import { useRouter } from "next/navigation";
 
 const AddPostPage = () => {
-  const { session, user } = useUser();
+  const router = useRouter();
+  const { session, user, loading } = useUser();
 
-  if (!session)
-    return (
-      <main className="flex justify-center items-center h-screen">
-        <p>
-          <a href="/user/login" className="text-blue-500 hover:underline">
-            ログインしてね！
-          </a>
-        </p>
-      </main>
-    );
+  if (loading) return <FullScreenLoader loading />;
+
+  if (loading && !session) {
+    router.push("/");
+  }
 
   return (
-    <main className="max-w-md mx-auto p-6">
-      <p className="text-2xl font-bold mb-4">Your Post</p>
+    <>
+      {user && (
+        <main className="max-w-md mx-auto p-6">
+          <p className="text-2xl font-bold mb-4">Your Post</p>
 
-      {user?.posts.length != 0 ? (
-        user?.posts.map((p: PostType, index: number) => (
-          <div key={index} className="border-b border-gray-200 py-4">
-            <p className="text-lg font-semibold">{p.title}</p>
-            <p className="text-gray-600">{p.content}</p>
+          {user?.posts.length != 0 ? (
+            user?.posts.map((p: PostType, index: number) => (
+              <div key={index} className="border-b border-gray-200 py-4">
+                <p className="text-lg font-semibold">{p.title}</p>
+                <p className="text-gray-600">{p.content}</p>
 
-            {/* 編集ページへのリンク */}
-            <Link
-              href={`/post/${p.id}`}
-              className="text-blue-500 hover:underline mt-2 inline-block"
-            >
-              編集
-            </Link>
-          </div>
-        ))
-      ) : (
-        <p>post not found</p>
+                {/* 編集ページへのリンク */}
+                <Link
+                  href={`/post/${p.id}`}
+                  className="text-blue-500 hover:underline mt-2 inline-block"
+                >
+                  編集
+                </Link>
+              </div>
+            ))
+          ) : (
+            <p>post not found</p>
+          )}
+        </main>
       )}
-    </main>
+    </>
   );
 };
 
